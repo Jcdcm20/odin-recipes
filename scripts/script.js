17fcd825f5610c013b1bf83d19293ea4573e725e
Julio Moreira <jcdcm20@gmail.com>
2022-12-18 20:37:52 -0300

    Update card behavior

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,24 @@
-const cards = document.querySelectorAll('.card');
+const links = document.querySelectorAll('a');
 
-cards.forEach(card => {
+links.forEach(link => {
+    const card = link.querySelector('.card');
     const title = card.querySelector('h2');
 
     card.addEventListener('mouseover', () => {
         title.style.color = 'red';
+
         card.style.backgroundColor = '#eeeeee';
+        
+        link.style.transform = 'scale(1.1)';
+        link.style.zIndex = 1;
     })
 
     card.addEventListener('mouseout', () => {
         title.style.color = '#000000';
+
         card.style.backgroundColor = '#ffffff';
+        
+        link.style.transform = 'scale(1)';
+        link.style.zIndex = 0;
     })
 })
