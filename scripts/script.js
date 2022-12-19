6f5943ac57a82fcd103efbb02ed9a76faca82ac7
Julio Moreira <jcdcm20@gmail.com>
2022-12-19 18:03:41 -0300

    Update functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,32 @@
 const links = document.querySelectorAll('a');
 
+
+
 links.forEach(link => {
     const card = link.querySelector('.card');
     const title = card.querySelector('h2');
 
-    card.addEventListener('mouseover', () => {
+    function hover() {
         title.style.color = 'red';
-
+    
         card.style.backgroundColor = '#eeeeee';
-        
+    
         link.style.transform = 'scale(1.1)';
         link.style.zIndex = 1;
-    })
-
-    card.addEventListener('mouseout', () => {
+    }
+    
+    function mouseAway() {
         title.style.color = '#000000';
-
+    
         card.style.backgroundColor = '#ffffff';
-        
+    
         link.style.transform = 'scale(1)';
         link.style.zIndex = 0;
-    })
+    }
+
+
+    card.addEventListener('mouseover', hover)
+
+    card.addEventListener('mouseout', mouseAway)
 })
+
